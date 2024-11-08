2f0f6202c067d14e36571c858464ee02cba97b66
Dmitry Morozov <mdv.sec@gmail.com>
2024-11-08 15:56:00 +0100

    Add argument validation for -p option

--- main.c
+++ main.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+static void print_usage(const char* name) {
+    fprintf(stderr, 
+            "Usage: %s -p <PID>\n",
+            name);
+}
 
 int main(int argc, char** argv) {
-    return 0;
+    int opt;
+    pid_t pid = 0;
+
+    while ((opt = getopt(argc, argv, "p:")) > 0) {
+        switch (opt) {
+            case 'p':
+                pid = atoi(optarg);
+                if (pid <= 0) {
+                    print_usage(argv[0]);
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            case '?':
+            default:
+                print_usage(argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    if (!pid) {
+        print_usage(argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
 }
