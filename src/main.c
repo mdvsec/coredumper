c88094d30f2747dae96292101350cfb591bb5844
Dmitry Morozov <mdv.sec@gmail.com>
2024-11-09 16:15:28 +0100

    Add basic process control via kill()

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <errno.h>
 
 static void print_usage(const char* name) {
     fprintf(stderr, 
@@ -8,8 +10,29 @@ static void print_usage(const char* name) {
             name);
 }
 
+static void handle_kill_error(const int pid) {
+    switch (errno) {
+        case EPERM:
+            fprintf(stderr, 
+                    "Access denied: root privileges are required to control process %d\n", 
+                    pid);
+            break;
+        case ESRCH:
+            fprintf(stderr, 
+                    "Error: process %d or process group cannot be found\n", 
+                    pid);
+            break;
+        default:
+            fprintf(stderr, 
+                    "Error: unknown error occurred while controlling process %d\n", 
+                    pid);
+            break;
+    }
+}
+
 int main(int argc, char** argv) {
     int opt;
+    int ret;
     pid_t pid = 0;
 
     while ((opt = getopt(argc, argv, "p:")) > 0) {
@@ -32,6 +55,22 @@ int main(int argc, char** argv) {
         print_usage(argv[0]);
         exit(EXIT_FAILURE);
     }
+
+    ret = kill(pid, SIGSTOP);
+    if (ret < 0) {
+        handle_kill_error(pid);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("[DEBUG] Proccess %d has been stopped\n", pid);
+
+    ret = kill(pid, SIGCONT);
+    if (ret < 0) {
+        handle_kill_error(pid);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("[DEBUG] Process %d has been resumed\n", pid);
 
     return EXIT_SUCCESS;
 }
