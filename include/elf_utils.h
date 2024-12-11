5356f51f93012f47a637f703c8dc6e2a74149096
Dmitry Morozov <mdv.sec@gmail.com>
2024-12-11 15:00:52 +0100

    Combine logic for parsing process state into proc_parser

--- include/elf_utils.h
+++ include/elf_utils.h
@@ -3,7 +3,7 @@
 
 #include <stddef.h>
 #include <sys/types.h>
-#include "parser.h"
+#include "proc_parser.h"
 
 int write_elf_header(const int, const ssize_t);
 ssize_t write_elf_program_headers(const int, const maps_entry_t*, const pid_t);
--- include/mem_reader.h
+++
@@ -1,9 +0,0 @@
-#ifndef MEM_READER_H 
-#define MEM_READER_H
-
-#include <sys/types.h>
-#include <elf.h>
-
-int dump_memory_region(const int, const Elf64_Phdr*, const pid_t);
-
-#endif
